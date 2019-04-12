c6785d758b9fbc6a6a4480a838d753c150d3f242
Simon Koob <simon.koob@gmx.net>
2019-04-12 22:42:28 +0200

    Trying to change the number setup for the list numbers

--- termpaper.R
+++ termpaper.R
@@ -40,20 +40,24 @@ nmbs <- nmbs[nmbs != "Kein Bild hinterlegt"]
 nmbs2 <- matrix(gsub("[^0123456789,]", "", nmbs), ncol = 4, byrow = T)
 head(nmbs2)
 
-value <- gsub(",", ".", nmbs2)
+?gsub
+
+value <- gsub(".", ".", nmbs2)
+value[,3] <- gsub("(\\d.\\d)(\\d.\\d)", "\\1", value[,3])
+value[,3] <- gsub("(\\d)\\d", "\\1.\d[:punct]", value[,3])
 class(value) <- "numeric"
 head(value)
 
-nds2 <- html_nodes(url, xpath = '//*+[contains(concat( " ", @class, " " ), concat( " ", "detail_list_player", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "detail_list_player", " " ))]//a | //a+//a')
-
-name <- html_text(nds2)
-head(name)
-
-dat <- data.frame(name, value)
-colnames(dat) <- c("Player ID", "Player Name", "Total Prize Money in 2019", "Overall Prize Money", "Share of 2019's prize money")
-dat
-
-#API tryout
-high_earn <- fromJSON('https://api.esportsearnings.com/v0/LookupHighestEarningTeams?apikey=27a22c5b980f900cb6d5b7d0c3d9d548daba6a57abdec2d115919ef8b01745bd&offset=0')
-
-head(high_earn)
+# nds2 <- html_nodes(url, xpath = '//*+[contains(concat( " ", @class, " " ), concat( " ", "detail_list_player", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "detail_list_player", " " ))]//a | //a+//a')
+# 
+# name <- html_text(nds2)
+# head(name)
+# 
+# dat <- data.frame(name, value)
+# colnames(dat) <- c("Player ID", "Player Name", "Total Prize Money in 2019", "Overall Prize Money", "Share of 2019's prize money")
+# dat
+# 
+# #API tryout
+# high_earn <- fromJSON('https://api.esportsearnings.com/v0/LookupHighestEarningTeams?apikey=27a22c5b980f900cb6d5b7d0c3d9d548daba6a57abdec2d115919ef8b01745bd&offset=0')
+# 
+# head(high_earn)
