07189b2f951ec5222c271817b59b60840316b8f5
Simon Koob <simon.koob@gmx.net>
2019-04-12 22:20:49 +0200

    Getting lists for top100 Players 2018

--- termpaper.R
+++ termpaper.R
@@ -29,20 +29,18 @@ plot(res)
 
 #getting lists of top players and games/teams
 
-url <- read_html("https://www.esportsearnings.com/history")
+url <- read_html("https://www.esportsearnings.com/history/2018/top_players")
 
 nds <- html_nodes(url, xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "content_main", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "highlight", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "detail_list_prize", " " ))]')
 
 nmbs <- html_text(nds)
-head(nmbs)
+head(nmbs) #income list top100 players 2018
 
 nmbs <- nmbs[nmbs != "Kein Bild hinterlegt"]
-nmbs2 <- matrix(gsub("[^0123456789,]", "", nmbs), ncol = 3, byrow = T)
+nmbs2 <- matrix(gsub("[^0123456789,]", "", nmbs), ncol = 4, byrow = T)
 head(nmbs2)
 
 value <- gsub(",", ".", nmbs2)
-value[,3] <- gsub("(\\d.\\d)(\\d.\\d)", "\\1", value[,3])
-value[,3] <- gsub("(\\d)\\d", "\\1", value[,3])
 class(value) <- "numeric"
 head(value)
 
@@ -54,3 +52,8 @@ head(name)
 dat <- data.frame(name, value)
 colnames(dat) <- c("Player ID", "Player Name", "Total Prize Money in 2019", "Overall Prize Money", "Share of 2019's prize money")
 dat
+
+#API tryout
+high_earn <- fromJSON('https://api.esportsearnings.com/v0/LookupHighestEarningTeams?apikey=27a22c5b980f900cb6d5b7d0c3d9d548daba6a57abdec2d115919ef8b01745bd&offset=0')
+
+head(high_earn)
