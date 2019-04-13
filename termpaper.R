d094fc5534868093f595e144dac17ace39fa6bf2
Simon Koob <simon.koob@gmx.net>
2019-04-13 11:15:43 +0200

    Starting out looping

--- termpaper.R
+++ termpaper.R
@@ -15,6 +15,7 @@ library(jsonlite)
 library(RSocrata)
 library(tidyverse)
 library(magrittr)
+library(stringr)
 
 ?gtrends
 
@@ -56,7 +57,32 @@ head(value)
 # dat <- data.frame(name, value)
 # colnames(dat) <- c("Player ID", "Player Name", "Total Prize Money in 2019", "Overall Prize Money", "Share of 2019's prize money")
 # dat
-# 
+
+
+#trying to loop over pages for top games
+
+npages <- 21
+game_prizes <- data.frame()
+
+for(i in 1998:2018) {
+  url <- paste0("https://www.esportsearnings.com/history/",i,"/games", sep = "")
+  src <- read_html(url)
+  
+  nds <- html_nodes(src, xpath = '//div/div/div/div/div/dl/dd')
+  nmbs <- html_text(nds)
+  nmbs <- nmbs[nmbs != "Kein Bild hinterlegt"]
+  nmbs2 <- matrix(gsub("[^0123456789,]", "", nmbs), ncol = 2, byrow = T)  
+  
+  nds2 <- html_nodes(src, xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "detail_list_player", " " ))] | //*+[contains(concat( " ", @class, " " ), concat( " ", "detail_list_player", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "detail_list_prize", " " ))]')
+  loc <- html_text(nds2)
+  
+  part <- data.frame(nmbs2, loc)
+  game_prizes <- rbind(game_prizes, part)
+  
+}
+  
+str(game_prizes)
+  
 # #API tryout
 # high_earn <- fromJSON('https://api.esportsearnings.com/v0/LookupHighestEarningTeams?apikey=27a22c5b980f900cb6d5b7d0c3d9d548daba6a57abdec2d115919ef8b01745bd&offset=0')
 # 
